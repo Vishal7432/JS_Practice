1316637395a229c6e63faa92c8213068fe2c441d
Vishal Gupta <vishalgupta10852@gmail.com>
2026-02-09 19:22:18 +0530

    Learned js Advance staff

---
+++ 09_Classes_Oops/MathPi.js
@@ -0,0 +1,33 @@
+const descriptor = Object.getOwnPropertyDescriptor(Math, "PI");
+// console.log(descriptor);
+
+// console.log(Math.PI);
+
+// const pi = Math.PI;
+// console.log(Math.PI);
+
+const chai = {
+  name: "ginger chai",
+  price: 250,
+  isAvailable: true,
+
+  orderChai: function () {
+    console.log("chai nhi bni");
+  },
+};
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, "name", {
+  //   writable: false,
+  //   enumerable: false,
+  enumerable: true,
+});
+
+// console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (const [key, value] of Object.entries(chai)) {
+  if (typeof value !== "function") {
+    console.log(`${key} : ${value}`);
+  }
+}
